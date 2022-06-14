e2bd9c26dbf2c5b35d42292e411dedefbdb5d57b
Andres Duarte M <andres.duarte.marquez@gmail.com>
2022-06-13 22:02:55 -0400

    add programs

--- ubuntu-22.04-post-installer.sh
+++ ubuntu-22.04-post-installer.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 sudo apt-get update
 sudo ubuntu-drivers autoinstall
-sudo apt-get install ubuntu-restricted-extras gufw zsh vim git curl mysql-server preload \
-htop neofetch terminator gimp vlc psensor gdebi bleachbit -y
+sudo apt-get install ubuntu-restricted-extras gufw ssh openssh-server nmap net-tools zsh vim git curl \
+htop neofetch preload terminator gdebi mysql-server gimp vlc psensor bleachbit -y
 sudo apt-get clean autoclean autoremove -y
 sudo systemctl start mysql.service
 sudo snap install spotify slack dbeaver-ce postman obs-studio
