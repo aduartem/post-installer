e2afa28ff2817e6dcebd8fbffc753e653c9a87bb
Andres Duarte M <andres.duarte.marquez@gmail.com>
2022-06-14 17:18:10 -0400

    add python3-pip

--- ubuntu-22.04-post-installer.sh
+++ ubuntu-22.04-post-installer.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 sudo ubuntu-drivers autoinstall
 sudo apt-get install ubuntu-restricted-extras gufw ssh openssh-server nmap net-tools zsh vim git curl \
-htop neofetch preload terminator gdebi mysql-server gimp vlc psensor bleachbit -y
+htop neofetch preload terminator gdebi python3-pip mysql-server gimp vlc psensor bleachbit -y
 sudo apt-get clean autoclean autoremove -y
 sudo systemctl start mysql.service
 sudo snap install spotify slack dbeaver-ce postman obs-studio
