034da2b81d64a918893947d2a324c4d08306a69b
Andres Duarte M <andres.duarte.marquez@gmail.com>
2022-06-16 00:07:15 -0400

    add programs

--- ubuntu-server-22.04-post-installer.sh
+++ ubuntu-server-22.04-post-installer.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo apt update
-sudo apt install ssh openssh-server nmap net-tools zsh vim git curl htop neofetch python3-pip mysql-server -y
+sudo apt install ssh openssh-server nmap net-tools zsh vim git tree wget curl htop neofetch python3-pip mysql-server -y
 sudo apt clean autoclean -y
 sudo systemctl start mysql.service
